60f831bd19fdbcb45a0b049b4fd7bbda9eb08c4a
poplink <2219535001@qq.com>
2021-10-04 00:16:05 +0800

    账号初始化模板修改

--- authorInit.js
+++ authorInit.js
@@ -21,259 +21,67 @@ function loadJSON(d) {
   for (let i = 0; i < d.length; i++) d[i] = JSON.parse(d[i]);
   return d;
 }
-let templateStart = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title></title>
-    <link href="./comment/MessageBoard.css" rel="stylesheet" />
-  </head>
+var fs = require("fs");
+var config = require("./constant");
 
-  <body>
-    <div id="app">
-      <div class="head">
-        <div
-          clsss="head-content"
-          style="display: flex; justify-content: space-between; align-items: center;"
-        >
-          <div class="head-left">
-            <div class="pl-div">
-              <img class="pl-img" :src="headUrl" alt="博主头像" />
-            </div>
-            <div>{{ author }}</div>
-          </div>
+/**
+ * user 账户的批量初始化
+ * 只能执行一次
+ */
+function userInit() {
+  fs.readFile("./resource/data2.json", function (err, data) {
+    if (err) console.error(err);
+    else {
+      let d = JSON.parse(data);
+      let temp = {};
+      let flag = false;
+      let result = d.filter((item, index) => {
+        flag = true;
+        for (let i = index + 1; i < d.length; i++) {
+          if (item.article_author_name == d[i].article_author_name) flag = false;
+        }
+        return flag;
+      })
+      result.forEach((item, index) => {
+        sql = "call proc_userByInsert2(?,'123456',?,@result)"
+        connection.query(sql
+          , [item.article_author_name, item.article_author_avatar_url]
+          , function (error, results) {
+            if (error) throw error;
+          });
+      });
+    }
+  });
+}
 
-          <div class="head-right">
-            <button class="pl-btn follow" @click="followStatus = !followStatus">
-              {{ followStatus ? "+ 关注" : "✔ 已关注" }}
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="main">
-`;
-let templateEnd = `
-      </div>
-      <div class="footer">
-        <div class="comment">
-          <!-- <post-comment> 一定要和 <message> 解耦，不能相互嵌套！ -->
-          <post-comment
-            @add="addMessage"
-            :remote-base-url="remoteBaseUrl"
-            :new-id="newId"
-            :commentator-id="userId"
-            :commentator-name="userName"
-            :commentator-head-url="userHeadUrl"
-          ></post-comment>
-          <div class="main">
-            <message
-              v-for="(item,index) in list"
-              :key="index"
-              :id="item.comment_id"
-              :content="item.content"
-              :create-time="item.create_time"
-              :son="item.son"
-              :remote-base-url="remoteBaseUrl"
-              :name="item.commentator_name"
-              :head-url="item.commentator_head_url"
-              :new-id="newId"
-              :commentator-id="userId"
-              :commentator-name="userName"
-              :commentator-head-url="userHeadUrl"
-            ></message>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
+/**
+ * 文章的批量初始化
+ * 只能执行一次
+ */
+function articleInit() {
+  fs.readFile("./resource/data2.json", function (err, data) {
+    if (err) console.error(err);
+    else {
+      let d = JSON.parse(data);
+      let result = d;
+      // result.forEach((item,index) => {
+      //   sql = "call proc_userByInsert(?,'123456',@result)"
+      //   connection.query(sql
+      //     ,[item.onwer_name]
+      //     ,function (error, results) {
+      //       if (error) throw error;
+      //     });
+      // });
+      /**
+       * 用一种类似递归的方式去等待异步完成
+       * 也可以使用 async / await
+      */
+      init(result, 0);
+    }
+  });
+}
 
-  <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-  <script src="./lib/vue@3.2.6.js"></script>
-  <script type="module">
-    import config from "./lib/config.js";
-    import Message from "./comment/Message.js";
-    import PostComment from "./comment/PostComment.js";
-    const article = {
-      data() {
-        return {
-          headUrl: config.defaultHeadBaseUrl + "default_head.png",
-          followStatus: false,
-          author: "",
-          authorHeadUrl: "",
-          userName: "",
-          remoteBaseUrl: "",
-          list: "",
-          newId: "",
-          userId: "",
-          userHeadUrl: "",
-        };
-      },
-      created() {
-        this.initData();
-        this.initCommentData();
-      },
-      methods: {
-        initData() {
-          this.author = localStorage.getItem("author")
-            ? localStorage.getItem("author")
-            : "error";
-          this.authorHeadUrl = localStorage.getItem("authorHeadUrl")
-            ? localStorage.getItem("authorHeadUrl")
-            : "http://116.63.152.202:5002/userHead/default_head.png";
-          this.userName = localStorage.getItem("userName")
-            ? localStorage.getItem("userName")
-            : null;
-          this.userId = localStorage.getItem("userId")
-            ? localStorage.getItem("userId")
-            : null;
-          this.userHeadUrl = localStorage.getItem("userHeadUrl")
-            ? localStorage.getItem("userHeadUrl")
-            : "";
-          this.newId = localStorage.getItem("newId")
-            ? localStorage.getItem("newId")
-            : null;
-          this.remoteBaseUrl = config.baseUrl ? config.baseUrl : "";
-        },
-        initCommentData() {
-          axios
-            .get(config.baseUrl + "/getCommentData" + "?new_id=" + this.newId)
-            .then((res) => {
-              let content = res.data.data;
-              content.forEach((item, index) => {
-                content[index].son = [];
-              });
-              let data = [];
-              let key = [];
-              let index = 0;
-              content.forEach((item) => {
-                if (item.parent_id == 0) {
-                  data.push(item);
-                  console.log(item.comment_id);
-                  key[item.comment_id] = index;
-                  index++;
-                } else {
-                  console.log(item.parent_id,key[1]);
-                  if (key[item.parent_id] == undefined) {
-                    for (let i = 0; i < key.length; i++) {
-                      if (key[i] != undefined) {
-                        data[key[i]].son.forEach((element) => {
-                          if (element.comment_id == item.parent_id)
-                            data[key[i]].son.push(item);
-                        });
-                      }
-                    }
-                  } else data[key[item.parent_id]].son.push(item);
-                }
-              });
-              this.list = data;
-              this.flag = true;
-            })
-            .catch((err) => {
-              console.log(err);
-            });
-        },
-        addMessage(e) {
-          this.list.push(e);
-        },
-      },
-    };
-
-    const app = Vue.createApp(article);
-    app.component("message", Message);
-    app.component("PostComment", PostComment);
-    app.mount("#app");
-  </script>
-
-  <style>
-    body,
-    html {
-      height: 100%;
-      margin: 0;
-    }
-    :root {
-      -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    }
-    #app {
-      padding: 10px;
-      font-family: Microsoft YaHei, Avenir, Helvetica, Arial, sans-serif;
-    }
-    .head-content {
-      /* head-content css 代码未生效，原因未知*/
-      display: flex;
-      justify-content: space-between;
-    }
-    .head-right {
-      display: flex;
-    }
-    .head-left {
-      display: flex;
-    }
-    .pl-btn {
-      border: 0.3em solid #48d597;
-      border-radius: 0.8em;
-      padding: 5px 10px;
-      margin: 5px;
-      font-weight: 600;
-      font-size: 14px;
-      background-color: #ffffff;
-      cursor: pointer;
-      background: #fff;
-      text-align: center;
-      color: white;
-      outline: 0;
-    }
-    .pl-btn.follow {
-      background-color: #48d597;
-    }
-    .pl-div {
-      position: relative;
-      width: 40px;
-      height: 40px;
-      margin-right: 15px;
-      border-radius: 10px;
-      outline: 0;
-      box-shadow: 1px 1px 4px 2px rgba(0, 0, 0, 0.1);
-    }
-    .pl-img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      border-radius: 10px;
-    }
-    .main {
-      padding: 10px 0px;
-    }
-    .internal {
-      font-weight: bold;
-      color: black;
-    }
-  </style>
-</html>
-`;
-
-fs.readFile("./resource/JSON.txt", function (err, data) {
-  if (err) console.error(err);
-  else {
-    let d = line(data.toString());
-    let result = loadJSON(d);
-    // result.forEach((item,index) => {
-    //   sql = "call proc_userByInsert(?,'123456',@result)"
-    //   connection.query(sql
-    //     ,[item.onwer_name]
-    //     ,function (error, results) {
-    //       if (error) throw error;
-    //     });
-    // });
-    /**
-     * 用一种类似递归的方式去等待异步完成
-     * 也可以使用 async / await
-    */
-    init(result, 0);
-  }
-});
+articleInit();
 
 function init(d, i) {
   if (i >= d.length) {
@@ -285,20 +93,20 @@ function init(d, i) {
       if (error) throw error;
       let fileName = "./html/" + results[0]["max(new_id)"] + ".html";
       console.log(fileName);
-      let template = templateStart + d[i].content + templateEnd;
+      let template = config.templateStart + d[i].article_content + config.templateEnd;
       fs.writeFile(fileName, template, (err) => {
         if (err) console.error(err);
       });
       sql = "select user_id from user where user_name = ?";
       let user_id;
-      connection.query(sql, [d[i].onwer_name], function (error, results) {
+      connection.query(sql, [d[i].article_author_name], function (error, results) {
         if (error) throw error;
         user_id = results[0].user_id;
         sql =
-          "call proc_newByInsert(?,'http://116.63.152.202:5002/userHead/default_head.png',?)";
+          "call proc_newByInsert(?,?,?)";
         connection.query(
           sql,
-          [user_id, d[i].name],
+          [user_id, d[i].article_cover, d[i].article_title],
           function (error, results) {
             if (error) throw error;
             i++;
